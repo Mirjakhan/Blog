af41f070fedf4e83aa2343db133deb683a33afa4
Mirjakhan <123504666+Mirjakhan@users.noreply.github.com>
2024-05-28 18:05:35 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Blog Website
+
+## Description
+A blog website built with Flask, where users can register, log in, create posts, and view posts by all users. This project demonstrates basic CRUD operations and user authentication.
+
+## Features
+- User registration and login
+- Create and view blog posts
+
+## Installation
+
+### Clone the repository
+```bash
+git clone https://github.com/your-username/blog-website.git
+cd blog-website
---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, redirect, url_for, flash, request, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from forms import RegistrationForm, LoginForm, PostForm
+from models import User, Post
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            session['user_id'] = user.id
+            flash('Login successful!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('You have been logged out!', 'info')
+    return redirect(url_for('index'))
+
+@app.route('/post/new', methods=['GET', 'POST'])
+def new_post():
+    if 'user_id' not in session:
+        flash('Please log in to create a post', 'warning')
+        return redirect(url_for('login'))
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, user_id=session['user_id'])
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('index'))
+    return render_template('post.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
